ce5ef621600a05d94b57dd11dea48b4b100d9887
marianderias <125551185+marianderias@users.noreply.github.com>
2023-02-18 16:36:49 +1100

    initial

---
+++ build/precache-manifest.525e36102a7e668141e8cb887ec65836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc6e29f0040231b26bc3",
+    "url": "http//marianderias.github.io/marianderias.github.io/static/css/main.4a07f26a.chunk.css"
+  },
+  {
+    "revision": "dc6e29f0040231b26bc3",
+    "url": "http//marianderias.github.io/marianderias.github.io/static/js/main.4d7065a5.chunk.js"
+  },
+  {
+    "revision": "f8bee6ded902cc5f8376",
+    "url": "http//marianderias.github.io/marianderias.github.io/static/js/runtime~main.fc4d2a9b.js"
+  },
+  {
+    "revision": "4a8226091ce3ae38eb9b",
+    "url": "http//marianderias.github.io/marianderias.github.io/static/js/2.28948fa1.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "http//marianderias.github.io/marianderias.github.io/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "e8c9f647151798a07b53b6ac5ce9f2f0",
+    "url": "http//marianderias.github.io/marianderias.github.io/index.html"
+  }
+];
---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import Cursor from "./Cursor";
+import Navbar from "./Navbar";
+import TypeWriter from './TypeWriter';
+import TaskBar from "./TaskBar";
+
+const Home = () => {
+  
+  const [cursorPos, setCursorPos] = useState({x:0, y:0});
+
+  // Check runs all the time to reset cursor position and add new message to 'typed' variables'
+  useEffect(() => {
+    const mousemove = (e) =>{
+      setCursorPos({x: e.clientX, y: e.clientY});
+    }
+    
+    window.addEventListener('mousemove', mousemove);
+    
+    return() => {
+      window.removeEventListener('mousemove', mousemove);
+    }
+  })
+
+  return (
+    <div className="home">
+      <Navbar />
+      <TypeWriter/>
+      <TaskBar/>
+      <Cursor x={cursorPos.x} y={cursorPos.y}/>
+    </div>
+    
+    
+  );
+ 
+}
+
+export default Home;
