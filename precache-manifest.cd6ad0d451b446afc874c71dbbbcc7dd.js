dd8f47413aeb8bb4e22f4ea0bf6dab85470a5dbf
marianderias <dumondpurple@hotmail.com>
2023-02-18 17:06:41 +1100

    Updates

---
+++ precache-manifest.cd6ad0d451b446afc874c71dbbbcc7dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69791754c673252c9dba",
+    "url": "/website/static/css/main.a9b85e95.chunk.css"
+  },
+  {
+    "revision": "69791754c673252c9dba",
+    "url": "/website/static/js/main.f1e1f346.chunk.js"
+  },
+  {
+    "revision": "51b64654c99f794d364a",
+    "url": "/website/static/js/runtime~main.b54eafdb.js"
+  },
+  {
+    "revision": "c32203bcfa4d909460de",
+    "url": "/website/static/js/2.bff19c49.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/website/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "7f734967a2090d70a9cc4221b032adbb",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "http//marianderias.github.io/website/precache-manifest.665237b035668f39e72bc40bcb38fb2a.js"
+  "/website/precache-manifest.cd6ad0d451b446afc874c71dbbbcc7dd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("http//marianderias.github.io/website/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
