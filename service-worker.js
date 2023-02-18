b9e342f6f3d3be254a571263bc0d0692f97a9c32
marianderias <dumondpurple@hotmail.com>
2023-02-18 17:23:21 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/public/index.html/precache-manifest.c612d87c4a5c4f56ba28e011070c472e.js"
+  "/website/precache-manifest.cd6ad0d451b446afc874c71dbbbcc7dd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/public/index.html/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
